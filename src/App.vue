532070d8b62bf59219df5a4f236ec29fe6402a97
astghik alexanian <astghik.alexanian@gmail.com>
2021-12-15 17:40:10 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <form class="card">
+      <!-- <h1>{{ getName }}</h1>
+      {{ setName() }} -->
+      {{ age }}
+      {{ name }}
+<!--
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+      </div>
+      <button class="btn primary">Submit</button> -->
+      <hello-world msg="Hello" />
+    </form>
+  </div>
+</template>
+<script>
+import { computed, ref, watch } from "vue";
+import Home from './views/Home';
+export default {
+  components: {
+    Home,
+  },
+  setup() {
+    let name = ref("Eduard");
+    let age = ref(21);
+    const getName = computed(() => {
+      return name.value + " Barev";
+    });
+    watch(age, () => {
+      name.value = 'Astghik'
+    })
+      console.log('Component is mounting');
+      name.value = 'SDAFGBGER'
+    const getAge = computed(() => {
+      return age;
+    });
+    const setName = () => {
+      name.value = 'Hayk'
+    }
+    const setAge = () => {
+      age.value = 30
+    }
+    return {
+      name,
+      getName,
+      setName,
+      getAge,
+      age,
+      setAge,
+    };
+  },
+};
+</script>
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: {
+      type: String,
+      required: false,
+      default: 'Goodby'
+    }
+  },
+  setup(props, { emit }) {
+    console.log(props.msg);
+    emit('sdavfsdvsd')
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42B983;
+}
+</style>
